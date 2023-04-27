62f1ffefe1c3fb8b6e219ee11ba890d643553cea
Joram Amador <jaab2020@mymail.pomona.edu>
2023-04-26 17:13:35 -0700

    initial commit!

---
+++ textSum.py
@@ -0,0 +1,68 @@
+"""
+    Text Summarization using IDF
+
+    Authors: David Yang, Kenny Gonzalez, Joram Amador
+"""
+
+import argparse
+import math
+
+# needs _ arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("training_data", help="data to train the model and get IDFs for words")
+parser.add_argument("test_data", help="data to run the model on and output sample summaries")
+parser.add_argument("start_sentences", help="number of sentences at start of paragraph to be weighted more")
+
+# parses arguments into args
+args = parser.parse_args()
+
+# save args
+training_data = args.training_data
+test_data = args.test_data
+start_sentences = int(args.start_sentences)
+
+def training(trainingFile):
+    """
+        Trains the model by going through the training 
+        documents and calculating the IDFs for the words
+
+        Returns:
+            IDFs (dict): dictionary containing the IDFs for each word
+
+        Parameters:
+            trainingFile (str): path to file with the documents to train on
+    """
+    wordCount = dict()
+    
+    training_docs = open(trainingFile, "r", encoding="utf-8")
+
+    # might have to do some preprocessing here to know what counts as docs?
+        # might have to lowercase all the words, maybe have a stoplist and remove the words?
+        
+    doc_count = 0
+    # get df for all words
+    for doc in training_docs:
+        doc_count += 1
+        inCurrDoc = set([])
+        doc_words = doc.split()
+        for word in doc_words:
+            if word not in inCurrDoc:
+                inCurrDoc.add(word)
+                if word not in wordCount:
+                    wordCount[word] = 1
+                else:
+                    wordCount[word] += 1
+    
+    # calculate IDF for all words
+    IDFs = dict()
+    for word in wordCount:
+        IDFs[word] = math.log(doc_count / wordCount[word])
+    return IDFs
+
+def main():
+    IDFs = training(training_data)
+    print(IDFs)
+
+
+if __name__ == "__main__":
+    main()
